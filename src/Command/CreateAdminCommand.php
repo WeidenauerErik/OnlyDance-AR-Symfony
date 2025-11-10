f585eed4b39800a925c0502df061c98cb7063750
Erik Weidenauer <erik.weidenauer@gmail.com>
2025-11-10 22:23:12 +0100

    adding more security to the backend

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'createAdmin',
+    description: 'Add a short description for your command',
+)]
+class CreateAdminCommand extends Command
+{
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $hasher)
+    {
+        parent::__construct();
+        $this->em = $em;
+        $this->hasher = $hasher;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = new User();
+        $user->setEmail('admin@example.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->hasher->hashPassword($user, '123456'));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $output->writeln('Admin user created!');
+        return Command::SUCCESS;
+    }
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,10 +9,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('api/user')]
+#[Route('api/crud/user')]
 final class UserController extends AbstractController
 {
     #[Route(name: 'app_user_index', methods: ['GET'])]
@@ -24,21 +23,13 @@ final class UserController extends AbstractController
     }
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $plainPassword = $form->get('password')->getData();
-            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
-            $user->setPassword($hashedPassword);
-
-            if (empty($user->getRoles())) {
-                $user->setRoles(['ROLE_USER']);
-            }
-
             $entityManager->persist($user);
             $entityManager->flush();
 
