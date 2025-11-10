90c3ee6ff071c989043884a333cf5443232f221f
Erik Weidenauer <erik.weidenauer@gmail.com>
2025-11-10 22:45:37 +0100

    updating new Routes prefix

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('api/crud/user')]
+#[Route('api/user')]
 final class UserController extends AbstractController
 {
     #[Route(name: 'app_user_index', methods: ['GET'])]
