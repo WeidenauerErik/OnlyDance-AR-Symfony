e8d79741f03209fffb36de7c8b27b63fc33719f2
Erik Weidenauer <erik.weidenauer@gmail.com>
2025-11-11 11:26:38 +0100

    adding login to my api Routes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[Route('/api')]
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
